6e6cc49939214145213397f5575be88ed7336ee9
kijato <kijato@gmail.com>
2020-10-12 09:47:31 +0200

    Add files via upload
    Update for PHP 7.

--- Excel/reader.php
+++ Excel/reader.php
@@ -28,7 +28,7 @@
 
 
 //require_once 'PEAR.php';
-require_once 'Spreadsheet/Excel/Reader/OLERead.php';
+require_once 'oleread.inc';
 //require_once 'OLE.php';
 
 define('SPREADSHEET_EXCEL_READER_BIFF8',             0x600);
@@ -256,9 +256,9 @@ class Spreadsheet_Excel_Reader
      *
      * Some basic initialisation
      */ 
-    function Spreadsheet_Excel_Reader()
+    function __construct()
     {
-        $this->_ole =& new OLERead();
+        $this->_ole = new OLERead();
         $this->setUTFEncoder('iconv');
     }
 
